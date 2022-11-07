856d3979f8d503c9ff365384f6ebf207662a3bd4
filipesouza2017 <filipemellosouza@gmail.com>
2022-11-07 07:26:33 -0300

    aula 1

---
+++ ConsoleDeTreinamento/ConnectionFactory.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDeTreinamento
+{
+    class ConnectionFactory
+    {
+        public static DateTime LastServiceRecovery { get; set; }
+        public static TimeSpan ServiceRecoveryTime { get; set; }
+        public static IOrganizationService Service { get; set; }
+
+        public static IOrganizationService GetConnection()
+        {
+            ServiceRecoveryTime = new TimeSpan(0, 1, 0);
+
+            if (Service == null)
+            {
+                GetCrmServiceClient();
+            }
+            else
+            {
+                TimeSpan timeDifference = DateTime.Now - LastServiceRecovery;
+
+                if(timeDifference.TotalMinutes > ServiceRecoveryTime.TotalMinutes)
+                {
+                    Service = null;
+                    GetCrmServiceClient();
+                }
+            }
+
+            return Service;
+        }
+
+        private static void GetCrmServiceClient()
+        {
+            LastServiceRecovery = DateTime.Now;
+
+            //CrmServiceClient.MaxConnectionTimeout = new TimeSpan(1, 0, 0);
+
+            CrmServiceClient serviceClient = new CrmServiceClient(
+                "AuthType=OAuth;" +
+                "Username=admin@treinamentofgvmdb.onmicrosoft.com;" +
+                "Password=P@ssw0rd;" +
+                "Url=https://mibfgv.crm2.dynamics.com/;" +
+                "AppId=76c8f2b5-dd99-468a-b1fa-d7a501166b9f;" +
+                "RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97;"
+            );
+            
+            Service = serviceClient;
+        }
+    }
+}
---
+++ ConsoleDeTreinamento/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDeTreinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOrganizationService service = ConnectionFactory.GetConnection();
+
+            int totalDeOportunidades = 1;
+            string cnpj = "12.1254.214/0001-55";
+            string nomeDaConta = "Hello World! 2";
+
+            //Tipos Picklist
+            OptionSetValue nivelDoCliente = new OptionSetValue(863260002);
+            Money valorTotalDeOportunidades = new Money(
+                new decimal(1.300)
+            );
+
+            EntityReference contatoPrimario = new EntityReference(
+                "contact",
+                new Guid("7469fd95-c0bd-4236-90bf-1d1100291df5")
+            );
+
+            Guid customId = Guid.NewGuid();
+
+            Console.WriteLine($"CUSTOMID: {customId}");
+
+            Entity conta = new Entity("account", customId);
+            conta["mib_cnpj"] = cnpj;
+            conta["name"] = nomeDaConta;
+            conta["mib_numerototaldeoportunidades"] = totalDeOportunidades;
+            conta["mib_niveldaconta"] = nivelDoCliente;
+            conta["mib_valortotaldeoportunidades"] = valorTotalDeOportunidades;
+            conta["primarycontactid"] = contatoPrimario;
+            Guid contaId = service.Create(conta);
+            Console.WriteLine(contaId);
+            Console.ReadKey();
+        }
+    }
+}
