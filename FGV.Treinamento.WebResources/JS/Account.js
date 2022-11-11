a02102b42f6b1e35ebacd18462165dc05ff04b6c
filipesouza2017 <filipemellosouza@gmail.com>
2022-11-11 17:30:27 -0300

    penúltima aula

---
+++ FGV.Treinamento.WebResources/JS/Account.js
@@ -0,0 +1,22 @@
+﻿if (typeof (FGV) == "undefined") { FGV = {} }
+if (typeof (FGV.Account) == "undefined") { FGV.Account = {} }
+
+FGV.Account = {
+    OnLoad: function (executionContext) {
+        var id = Xrm.Page.data.entity.getId();
+
+        Xrm.WebApi.online.retrieveMultipleRecords("systemuser", "?$select=businessunitid&$filter=systemuserid eq " + id).then(
+            function success(results) {
+                if (results.entities.length > 0) {
+                    if (results.entities[0]["businessunitid"] == "") {
+
+                    }
+                }
+            },
+            function (error) {
+
+            }
+        );
+
+    }
+}
