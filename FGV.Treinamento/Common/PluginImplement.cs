fba5c47d71c489ff7f2f7dff60840d25815900a6
filipesouza2017 <filipemellosouza@gmail.com>
2022-11-11 08:41:38 -0300

    Aula plugins

---
+++ FGV.Treinamento/Common/PluginImplement.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGV.Treinamento.Common
+{
+    public abstract class PluginImplement : IPlugin
+    {
+        public IPluginExecutionContext Context { get; set; }
+        public ITracingService TracingService { get; set; }
+        public IOrganizationServiceFactory ServiceFactory { get; set; }
+        public IOrganizationService Service { get; set; }
+
+        public enum StageNumbers
+        {
+            PreValidation = 10,
+            PreOperation = 20,
+            MainOperation = 30,
+            PostOperation = 40
+        }
+
+        public enum MessageNames
+        {
+            Create,
+            Update,
+            Delete
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            Context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            TracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            ServiceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            Service = ServiceFactory.CreateOrganizationService(Context.UserId);
+
+            try
+            {
+                ExecutePlugin(serviceProvider);
+            }
+            catch(Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.ToString());
+            }
+        }
+
+        public abstract void ExecutePlugin(IServiceProvider serviceProvider);
+    }
+}
--- FGV.Treinamento/Opportunities.cs
+++ FGV.Treinamento/Opportunities.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xrm.Sdk;
+﻿using FGV.Treinamento.Common;
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
@@ -8,38 +9,83 @@ using System.Threading.Tasks;
 
 namespace FGV.Treinamento
 {
-    public class Opportunities : IPlugin
+    public class Opportunities : PluginImplement
     {
-        public void Execute(IServiceProvider serviceProvider)
+        public override void ExecutePlugin(IServiceProvider serviceProvider)
         {
-            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            #region Exemplos com imagens
+            //Entity preImage = (Entity)this.Context.PreEntityImages["PreImage"];
+            //Entity preImage = (Entity)this.Context.PreEntityImages["PreImage"];
+            //Entity postImage = (Entity)this.Context.PostEntityImages["PostImage"];
 
-            Entity opportunity = (Entity)context.InputParameters["Target"];
+            //string messages = string.Empty;
 
-            tracingService.Trace("Recuperou registro da oportunidade");
+            //if (preImage != null)
+            //{
+            //    if (preImage.Contains("name"))
+            //        messages += $"Target: NAME: {preImage["name"].ToString()}";
+            //}
+
+            //throw new InvalidPluginExecutionException(messages);
+            #endregion
+
+            Entity opportunityPostImage = (Entity)this.Context.PostEntityImages["PostImage"];
+
+            Entity account = GetAccount(opportunityPostImage, new string[] { "mib_valortotaldeoportunidades" });
+
+            account["mib_valortotaldeoportunidades"] = new Money(
+                ((Money)account["mib_valortotaldeoportunidades"]).Value + 
+                ((Money)opportunityPostImage["totalamount"]).Value
+            );
+
+            this.Service.Update(account);
+
+            if (this.Context.MessageName == PluginImplement.MessageNames.Create.ToString() || this.Context.MessageName == PluginImplement.MessageNames.Delete.ToString())
+                CreateAndDelete();
+        }
+
+        private static string IncrementMessage(Entity opportunityTarget, string stage)
+        {
+            if (opportunityTarget.Contains("totalamount"))
+                return $"{stage}: {((Money)opportunityTarget["totalamount"]).Value.ToString()}";
+
+            return string.Empty;
+        }
+
+        private void CreateAndDelete()
+        {
+            Entity opportunity = this.Context.MessageName == PluginImplement.MessageNames.Create.ToString() ? (Entity)this.Context.InputParameters["Target"] : (Entity)this.Context.PreEntityImages["PreImage"];
+
+            this.TracingService.Trace("Recuperou registro da oportunidade");
 
             if (opportunity.Contains("parentaccountid"))
             {
-                EntityReference accountReference = (EntityReference)opportunity["parentaccountid"];
-
-                Entity account = service.Retrieve(
-                    accountReference.LogicalName,
-                    accountReference.Id,
-                    new ColumnSet("mib_numerototaldeoportunidades")
-                );
+                Entity account = GetAccount(opportunity, new string[] { "mib_numerototaldeoportunidades" });
 
                 int numeroTotalDeOportunidades = account.Contains("mib_numerototaldeoportunidades") ? (int)account["mib_numerototaldeoportunidades"] : 0;
 
-                numeroTotalDeOportunidades += 1;
+                if (this.Context.MessageName == PluginImplement.MessageNames.Create.ToString())
+                    numeroTotalDeOportunidades += 1;
+                else
+                    numeroTotalDeOportunidades -= 1;
 
-                tracingService.Trace($"Número total {numeroTotalDeOportunidades}");
+                this.TracingService.Trace($"Número total {numeroTotalDeOportunidades}");
 
                 account["mib_numerototaldeoportunidades"] = numeroTotalDeOportunidades;
-                service.Update(account);
+                this.Service.Update(account);
             }
+        }
+
+        private Entity GetAccount(Entity opportunity, string[] columns)
+        {
+            EntityReference accountReference = (EntityReference)opportunity["parentaccountid"];
+
+            Entity account = this.Service.Retrieve(
+                accountReference.LogicalName,
+                accountReference.Id,
+                new ColumnSet(columns)
+            );
+            return account;
         }
     }
 }
